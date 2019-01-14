1fb70cf687befc4d295cb80dfc9b8ee987710d84
zhucw@mengweima.com <zhucw@mengweima.com>
2019-01-14 10:47:17 +0800

    脚本自动更新测试

--- autoUpdate.js
+++ autoUpdate.js
@@ -1,16 +1,18 @@
-function 更新程序(){
-    var spid = "趣头条刷金币";//脚本识别id
-    var Edition = 3;//当前版本号
-    var url = "http://wj.aiselp.ml/index.php?share/fileDownload&user=1&sid=REf4bvdm";
+function update(scriptName,scriptVersion){
+    var url = "https://raw.githubusercontent.com/RyanPro/autojs_script/master/version.js";
     var str = http.get(url)
     str = JSON.parse(str.body.string());
-    if (str[spid].版本>Edition){
-        var f = engines.myEngine().cwd()+"/"+spid+"("+str[spid].版本+").js";
-        if (!files.exists(f)){
-        files.write(f,http.get(str[spid].url).body.string());
-        toast("脚本已更新，保存在当前脚本所在目录，请使用新的脚本");
-        } else toast("文件已存在");
-        exit();
-        } else if (str[spid].版本==Edition) toast("脚本已是最新");
+    for(var i = 0; i< str.length;i++){
+        var thisScript = str[i];
+        var name = thisScript.name;
+        var version = thisScript.version;
+        
+        if(scriptName == name && version != scriptVersion){
+            var path = "/sdcard/"+scriptName+".js";
+            var scriptContent = "https://raw.githubusercontent.com/RyanPro/autojs_script/master/"+scriptName+".js";
+            files.write(path,scriptContent);
+            return true;
+        }
+        return false;
     }
-    更新程序();
+}
